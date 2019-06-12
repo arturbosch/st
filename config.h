3d148c923dbf6c41fa226819d419c31e94839654
Artur Bosch <arturbosch@gmx.de>
2019-06-12 09:05:23 +0200

    Increase font size, remap scrolling keys to not interfer with nvim

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -237,14 +237,14 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_Control_L,   iso14755,       {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ MODKEY,               XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ MODKEY,               XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ MODKEY,               XK_k,           kscrollup,      {.i =  1} },
-	{ MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
-	{ MODKEY,               XK_Up,          kscrollup,      {.i =  1} },
-	{ MODKEY,               XK_Down,        kscrolldown,    {.i =  1} },
-	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
-	{ MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
+	{ MODKEY,               XK_Page_Up,     kscrollup,      {.i =  1} },
+	{ MODKEY,               XK_Page_Down,   kscrolldown,    {.i =  1} },
+	// { ControlMask|ShiftMask,XK_k,           kscrollup,      {.i =  1} },
+	// { ControlMask|ShiftMask,XK_j,           kscrolldown,    {.i =  1} },
+	// { ControlMask|ShiftMask,XK_Up,          kscrollup,      {.i =  1} },
+	// { ControlMask|ShiftMask,XK_Down,        kscrolldown,    {.i =  1} },
+	// { ControlMask|ShiftMask,XK_u,           kscrollup,      {.i = -1} },
+	// { ControlMask|ShiftMask,XK_d,           kscrolldown,    {.i = -1} },
 	{ TERMMOD,              XK_Up,          zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Down,        zoom,           {.f = -1} },
 	{ TERMMOD,              XK_K,           zoom,           {.f = +1} },
